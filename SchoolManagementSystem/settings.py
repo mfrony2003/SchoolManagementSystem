cd45ede9b4221fd5ce12c1dbfbb77bde0066bc1a
mfrony2003 <mfrony2003@gmail.com>
2023-12-14 12:59:18 +0600

    settings

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,7 +133,15 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# All settings common to all environments
+
+STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT=''
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
