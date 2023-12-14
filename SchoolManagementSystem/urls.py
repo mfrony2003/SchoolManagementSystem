9a579125d0040c1c4ec8661523ace020647373b4
mfrony2003 <mfrony2003@gmail.com>
2023-12-14 13:07:30 +0600

    STATIC

--- SchoolManagementSystem/urls.py
+++ SchoolManagementSystem/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,7 @@ urlpatterns = [
     path('',include('apps.Students.urls')),    
     path('',include('apps.Staffs.urls')),    
 ]
+
+
+
+urlpatterns += staticfiles_urlpatterns()
